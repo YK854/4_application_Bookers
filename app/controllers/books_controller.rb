2170094255f75a3d50cf9d85374f55833964a469
YK854 <95330893+YK854@users.noreply.github.com>
2021-12-12 14:49:48 +0000

    [add] 4_Bookers_2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,45 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to "/books/#{@book.id}"
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to "/books/#{book.id}"
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
